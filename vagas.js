a74dd98d1332038e4ea7fed8e8370ad4a6f86834
willians vieira <willians.vieira@acovisa.com.br>
2024-06-19 12:27:07 -0300

    atualizações para ser atualizado automatico

--- vagas.js
+++ vagas.js
@@ -1,28 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
     const apiUrl = 'https://sislojaestacionamentoapi.azurewebsites.net/estacionamento';
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const garagensContainer = document.getElementById('garagens');
-            garagensContainer.innerHTML = ''; // Clear any existing content
-            data.forEach(garagem => {
-                const garagemDiv = document.createElement('div');
-                garagemDiv.className = 'garagem';
+    function fetchGaragens() {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                const garagensContainer = document.getElementById('garagens');
+                garagensContainer.innerHTML = ''; // Clear any existing content
+                data.forEach(garagem => {
+                    const garagemDiv = document.createElement('div');
+                    garagemDiv.className = 'garagem';
 
-                const garagemName = document.createElement('h2');
-                garagemName.textContent = garagem.nome;
+                    const garagemName = document.createElement('h2');
+                    garagemName.textContent = garagem.nome;
 
-                const vagasDisponiveis = document.createElement('p');
-                vagasDisponiveis.textContent = garagem.vagasDisponiveis;
+                    const vagasDisponiveis = document.createElement('p');
+                    vagasDisponiveis.textContent = garagem.vagasDisponiveis;
 
-                garagemDiv.appendChild(garagemName);
-                garagemDiv.appendChild(vagasDisponiveis);
+                    garagemDiv.appendChild(garagemName);
+                    garagemDiv.appendChild(vagasDisponiveis);
 
-                garagensContainer.appendChild(garagemDiv);
+                    garagensContainer.appendChild(garagemDiv);
+                });
+            })
+            .catch(error => {
+                console.error('Erro ao buscar dados da API:', error);
             });
-        })
-        .catch(error => {
-            console.error('Erro ao buscar dados da API:', error);
-        });
+    }
+
+    // Atualiza a lista de garagens a cada 5 segundos
+    setInterval(fetchGaragens, 5000);
+
+    // Fetch data on initial load
+    fetchGaragens();
 });
