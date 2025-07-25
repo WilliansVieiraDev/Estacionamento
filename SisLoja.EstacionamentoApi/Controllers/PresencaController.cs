e92a878680b22e24ba236d1ece5d8ef3352f6ce3
willians vieira <willians.vieira@acovisa.com.br>
2025-07-25 11:37:08 -0300

    resolvido data e criado endpoint para o manual

--- SisLoja.EstacionamentoApi/Controllers/PresencaController.cs
+++ SisLoja.EstacionamentoApi/Controllers/PresencaController.cs
@@ -53,6 +53,43 @@ namespace SisLoja.EstacionamentoApi.Controllers
             }
         }
 
+        [HttpPost("registrar-manualmente")]
+        public async Task<IActionResult> RegistrarManualmente([FromBody] PresencaManualInput input)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(input.Nome))
+                    return BadRequest("Nome inválido.");
+
+                bool jaRegistrado = await _context.Presencas
+                    .AnyAsync(p => p.Nome == input.Nome && p.DataHora.Date == DateTime.UtcNow.Date);
+
+                if (jaRegistrado)
+                    return Ok(new { mensagem = $"Presença registrada para {input.Nome}." });
+
+                var dataUtc = DateTime.UtcNow.AddHours(-3); // Garante Kind = UTC
+
+                var novaPresenca = new Presenca
+                {
+                    Codigo = "0",
+                    Nome = input.Nome,
+                    DataHora = dataUtc
+                };
+
+                _context.Presencas.Add(novaPresenca);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { mensagem = $"Presença registrada para {input.Nome}." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+                throw;
+            }
+        }
+
+
         [HttpGet]
         public async Task<IActionResult> Listar()
         {
@@ -66,6 +103,11 @@ namespace SisLoja.EstacionamentoApi.Controllers
         public class PresencaInput
         {
             public string Codigo { get; set; }
+        }
+
+        public class PresencaManualInput
+        {
+            public string Nome { get; set; }
         }
     }
 }
